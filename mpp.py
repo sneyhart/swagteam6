5fd2e78ee26d59bb578a9d7f8aaeaa1ae9c2ee4d
Clark <Jclarkfree@gmail.com>
2017-04-28 15:27:44 -0400

    finished up testing, really bad results, need to debug mpp.py

--- mpp.py
+++ mpp.py
@@ -15,17 +15,17 @@ def cmv(x, y, mean, cov):
     return (pre_top/pre_bottom) * np.exp(post_scalar * post_matrix_mult)
 
 #function that returns Case 1 Discriminant
-def case_1(x, y, mean, prior):
-    return -np.dot(np.subtract(np.array([x,y]), mean),np.transpose(np.subtract(np.array([x,y]), mean))) + np.log(prior)
+def case_1(x, mean, prior):
+    return -np.dot(np.subtract(np.array(x), mean),np.transpose(np.subtract(np.array(x), mean))) + np.log(prior)
 
 #function that returns Case 2 Discriminant
-def case_2(x, y, mean, cov, prior):
-    return -np.dot(np.dot(np.transpose(np.subtract(np.array([x,y]), mean)), np.linalg.inv(cov)), np.subtract(np.array([x,y]), mean)) + np.log(prior)
+def case_2(x, mean, cov, prior):
+    return -np.dot(np.dot(np.transpose(np.subtract(np.array(x), mean)), np.linalg.inv(cov)), np.subtract(np.array(x), mean)) + np.log(prior)
 
 #function that returns Case 3 Discriminant
-def case_3(x, y, mean, cov, prior):
-    first = np.dot(np.dot(np.transpose(np.array([x,y])), np.divide(np.linalg.inv(cov), -2)), np.array([x,y]))
-    second = np.dot(np.transpose(np.dot(np.linalg.inv(cov), mean)), np.array([x,y]))
+def case_3(x, mean, cov, prior):
+    first = np.dot(np.dot(np.transpose(np.array(x)), np.divide(np.linalg.inv(cov), -2)), np.array(x))
+    second = np.dot(np.transpose(np.dot(np.linalg.inv(cov), mean)), np.array(x))
     third = (np.dot(np.dot(np.transpose(mean), np.linalg.inv(cov)),mean)/-2.0) + (np.log(np.linalg.det(cov))/-2.0)
     return first + second + third + np.log(prior)
 
@@ -45,4 +45,29 @@ classes = np.array([np.array(classer) for classer in classes])
 means = [np.mean(row) for row in classes]
 covariance = np.cov(cov_lines)
 
+testing_lines = [np.array((file_line.rstrip('\n').split(','))).astype(int) for file_line in open('poker-hand-testing.data')]
+testing_lines = np.array(testing_lines)
+tests = np.delete(testing_lines.T, testing_lines.T.shape[0] - 1,0).T
+actual_classes = testing_lines.T[-1]
 
+choices = []
+
+for t,test in enumerate(tests):
+    max_disc = -1
+    if t % 50000 == 0:
+        print "{}%".format(((t * 1.0)/10000))
+    max_val = -1000000
+    for i in range(0,10):
+        res = case_2(test, means[i],covariance,priors[i])
+        if res > max_val:
+            max_disc = i
+            max_val = res
+    choices.append(max_disc)
+
+corrects = 0
+total = 0
+for i in range(0, len(choices)):
+    if choices[i] == actual_classes[i]:
+        corrects = corrects + 1
+    total = total + 1
+print "Case 2 - {}".format((corrects * 1.0)/total)
