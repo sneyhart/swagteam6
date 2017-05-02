05628c6f72ffb906e7a19221b65967ab8fe2ff97
Sam Neyhart <sneyhart@vols.utk.edu>
2017-05-02 16:49:16 -0400

    Completed wta and started on kmeans

---
+++ kmeans.c
@@ -0,0 +1,76 @@
+/*
+ * kmeans.c
+ * Sam Neyhart
+ * This contains the kmeans program used in the final project of 
+ * ECE 471.
+ * USAGE: kmeans filename.data nclusters
+ */
+
+#include<string.h>
+#include<time.h>
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include"hand.h"
+
+
+static const int ITERATIONS = 1; //How many times to go through data
+static int NCLUSTERS;
+static int NLINES = 0;
+
+int main(int argc, char ** argv)
+{
+	//----------File IO---------
+	FILE * in;
+	in = fopen(argv[1],"r");
+	if (argc != 3) return 0;
+	NCLUSTERS = atoi(argv[2]);
+
+	//Get numLines
+	char buf[255];
+	while(fgets(buf,255,in) != NULL){
+		NLINES++;
+	}
+	rewind(in);
+
+	//create arrays
+	hand * data = malloc(10*NLINES*sizeof(hand));
+	hand * mu = malloc(10*NCLUSTERS*sizeof(hand));
+
+	//fill data
+	for(int i = 0; i < NLINES; i++){
+		char * tmp;
+		fgets(buf,255,in);
+		tmp = strtok(buf,",");
+		for(int j = 0; j < 10; j++){
+			data[i].cards[j] = atof(tmp);
+			tmp = strtok(NULL,",");
+		}
+	}
+
+	//randomly fill mu
+	time_t t;
+	srand((unsigned)time(&t));
+	for(int i = 0; i < NCLUSTERS; i++){
+		int tmp = rand() % NLINES;
+		for(int j = 0; j < 10; j++){
+			mu[i].cards[j] = data[tmp].cards[j];
+		}
+	}
+	
+	//---------Perform WTA----------
+	for(int i = 0; i < ITERATIONS; i++)
+		update_mu(&data[i],mu,NCLUSTERS);
+	//print mu	
+	for(int i = 0; i < NCLUSTERS; i++){
+		for(int j = 0; j < 10; j++){
+			printf("%lf ",mu[i].cards[j]);
+		}
+		printf("\n");
+	}
+
+	//---------Cleanup----------
+	free(data);
+	free(mu);
+	fclose(in);
+}
--- wta.c
+++ wta.c
@@ -1,3 +1,11 @@
+/*
+ * wta.c
+ * Sam Neyhart
+ * This file contains the wta program used in the final project of 
+ * ECE 471.
+ * USAGE: wta filename.data nclusters
+ */
+
 #include<string.h>
 #include<time.h>
 #include<stdio.h>
@@ -5,6 +13,10 @@
 #include<stdlib.h>
 #include"hand.h"
 
+
+static const int ITERATIONS = 1; //How many times to go through data
+static const int NCLASSES = 10;
+static const int NCARDS = 10;
 static int NCLUSTERS;
 static int NLINES = 0;
 
@@ -15,52 +27,72 @@ int main(int argc, char ** argv)
 	in = fopen(argv[1],"r");
 	if (argc != 3) return 0;
 	NCLUSTERS = atoi(argv[2]);
-
 	//Get numLines
 	char buf[255];
 	while(fgets(buf,255,in) != NULL){
 		NLINES++;
 	}
 	rewind(in);
-
 	//create arrays
-	hand * data = malloc(10*NLINES*sizeof(hand));
-	hand * mu = malloc(10*NCLUSTERS*sizeof(hand));
-
+	hand * data = malloc(NLINES*sizeof(hand));
+	hand * mu = malloc(NCLUSTERS*sizeof(hand));
+	int * clas = malloc(NLINES*sizeof(int));
 	//fill data
 	for(int i = 0; i < NLINES; i++){
 		char * tmp;
 		fgets(buf,255,in);
 		tmp = strtok(buf,",");
-		for(int j = 0; j < 10; j++){
+		for(int j = 0; j < NCARDS; j++){
 			data[i].cards[j] = atof(tmp);
 			tmp = strtok(NULL,",");
 		}
+		clas[i]=atoi(tmp);
 	}
-
 	//randomly fill mu
 	time_t t;
 	srand((unsigned)time(&t));
 	for(int i = 0; i < NCLUSTERS; i++){
 		int tmp = rand() % NLINES;
-		for(int j = 0; j < 10; j++){
+		for(int j = 0; j < NCARDS; j++){
 			mu[i].cards[j] = data[tmp].cards[j];
 		}
 	}
 	
 	//---------Perform WTA----------
-	for(int i = 0; i < NLINES; i++)
-		update_mu(&data[i],mu,NCLUSTERS);
-	//print mu	
+	for(int i = 0; i < ITERATIONS; i++){
+		for(int j=0; j<NLINES;j++){
+			update_mu(&data[j],mu,NCLUSTERS);
+		}
+	}
+	
+	//-----Deterimine Accuracy-----
+	int *clus = malloc(NLINES*sizeof(int));
+	cluster(data,mu,NLINES,NCLUSTERS,clus);
+	hand *avgclas = calloc(NCLASSES,sizeof(hand));
+	hand *avgclus = calloc(NCLUSTERS,sizeof(hand));
+	avg(avgclas, data, clas, NLINES, NCLASSES);
+	avg(avgclus, data, clus, NLINES, NCLUSTERS);
+	printf("Avg Clusters:\n");
 	for(int i = 0; i < NCLUSTERS; i++){
-		for(int j = 0; j < 10; j++){
-			printf("%lf ",mu[i].cards[j]);
+		for(int j = 0; j < NCARDS; j++){
+			printf("%lf ",avgclus[i].cards[j]);
 		}
 		printf("\n");
 	}
-
+	printf("\nAvg Classes:\n");
+	for(int i = 0; i < NCLASSES; i++){
+		for(int j = 0; j < NCARDS; j++){
+			printf("%lf ",avgclas[i].cards[j]);
+		}
+		printf("\n");
+	}
+	double acc;
+	acc = accuracy(NLINES,NCLUSTERS,NCLASSES,clus,clas,avgclus,avgclas);
+	printf("\nOverall Accuracy is %lf\n\n",acc);	
 	//---------Cleanup----------
 	free(data);
 	free(mu);
+	free(clas);
+	free(clus);
 	fclose(in);
 }
